583d7abb32dc0e59af19f011971b2c66821f34a8
GalaGale <nga_r@mail.ru>
2017-10-03 03:18:07 +0400

    1

--- init.sh
+++ init.sh
@@ -5,8 +5,9 @@ sudo mv /home/box/web/web/* /home/box/web
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo gunicorn -b 0.0.0.0:8080 hello:app
-#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn/gunicorn restart
+#sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn/gunicorn restart
 
 sudo /etc/inint.d/mysql start
